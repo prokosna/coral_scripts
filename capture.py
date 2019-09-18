b866175b78543030904cc66772b02b579b1b78d7
Toshiyuki (Topy) Isobe <isobe@google.com>
2019-09-18 10:04:06 +0000

    Add manual model

--- capture.py
+++ capture.py
@@ -3,6 +3,33 @@ import time
 
 import gstreamer
 import upload
+import threading
+
+
+class InputMonitor(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self._lock = threading.Lock()
+        self._is_key_pressed = False
+
+    def is_key_pressed(self):
+        self._lock.acquire()
+
+        if self._is_key_pressed:
+            self._is_key_pressed = False
+            self._lock.release()
+            return True
+
+        self._lock.release()
+
+        return False
+
+    def run(self):
+        while True:
+            input()
+            self._lock.acquire()
+            self._is_key_pressed = True
+            self._lock.release()
 
 
 def main():
@@ -11,18 +38,36 @@ def main():
     parser.add_argument('--path', help='GCS path prefix for uploading images')
     parser.add_argument('--fps', help='Capturing frames per second',
                         type=float, default=1)
+    parser.add_argument('--manual', help='Take a picture when key pressed',
+                        action='store_true')
     args = parser.parse_args()
 
-    print('FPS is {}'.format(args.fps))
+    if args.manual:
+        print('Press enter when you want to take a picture')
+    else:
+        print('FPS is {}'.format(args.fps))
+
     duration = 1.0 / args.fps
     last_time = time.monotonic()
 
+    input_monitor = InputMonitor()
+
     def user_callback(image):
+        nonlocal input_monitor
         nonlocal last_time
+
         start_time = time.monotonic()
-        if start_time - last_time > duration:
-            upload.upload(args.bucket, args.path, image)
-            last_time = time.monotonic()
+        if args.manual:
+            if input_monitor.is_key_pressed():
+                upload.upload(args.bucket, args.path, image)
+        else:
+            if start_time - last_time > duration:
+                upload.upload(args.bucket, args.path, image)
+                last_time = time.monotonic()
+
+    if args.manual:
+        input_monitor.start()
+        print('monitoring keyboard input...')
 
     gstreamer.run_pipeline(user_callback)
 

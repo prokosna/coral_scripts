4807a1dd030a9748a5fd949d5ac0611b49777d7f
prokosna <prokosna@gmail.com>
2019-10-25 00:27:11 +0900

    Add GPIO switch function to input.py

--- input.py
+++ input.py
@@ -1,9 +1,15 @@
 import threading
+import time
+
+from periphery import GPIO
 
 
 class InputMonitor(threading.Thread):
-    def __init__(self):
+    def __init__(self, gpio_pin=None):
         threading.Thread.__init__(self)
+        self._is_gpio = gpio_pin is not None
+        if self._is_gpio:
+            self._gpio = GPIO(gpio_pin, 'in')
         self._lock = threading.Lock()
         self._is_key_pressed = False
 
@@ -21,7 +27,15 @@ class InputMonitor(threading.Thread):
 
     def run(self):
         while True:
-            input()
-            self._lock.acquire()
-            self._is_key_pressed = True
-            self._lock.release()
+            if self._is_gpio:
+                v = self._gpio.read()
+                if v and not self._is_key_pressed:
+                    self._lock.acquire()
+                    self._is_key_pressed = True
+                    self._lock.release()
+                time.sleep(0.1)
+            else:
+                input()
+                self._lock.acquire()
+                self._is_key_pressed = True
+                self._lock.release()
